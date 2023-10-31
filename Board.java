f3f99d75b00ed27cfd35121309d70db5ef90463a
Sajeev Magesh <sajeevmagesh123@gmail.com>
2023-10-31 11:10:30 -0700

    added comments

--- Board.java
+++ Board.java
@@ -2,49 +2,59 @@ import java.util.Scanner;
 import java.io.File;
 
 public class Board {
-    private String solvedPhrase;
-    private String phrase;
-    private int currentLetterValue;
+    private String solvedPhrase;       // The phrase with solved letters
+    private String phrase;             // The original phrase to guess
+    private int currentLetterValue;    // The current value of a letter (unused)
 
+    // Constructor to initialize the Board object
     public Board() {
-        solvedPhrase = "";
-        phrase = loadPhrase();
-        currentLetterValue = 0; 
+        solvedPhrase = "";             // Initialize solvedPhrase as an empty string
+        phrase = loadPhrase();         // Load a random phrase from the file
+        currentLetterValue = 0;        // Initialize currentLetterValue to 0 (unused)
     }
 
+    // Getter method for retrieving the solved phrase
     public String getSolvedPhrase() {
         return solvedPhrase;
     }
 
+    // Getter method for retrieving the original phrase to guess
     public String getPhrase() {
         return phrase;
     }
 
+    // Getter method for retrieving the current letter value (unused)
     public int getCurrentLetterValue() {
         return currentLetterValue;
     }
 
+    // Setter method for setting the solved phrase
     public void setSolvedPhrase(String solvedPhrase) {
         this.solvedPhrase = solvedPhrase;
     }
 
+    // Setter method for setting the original phrase to guess
     public void setPhrase(String phrase) {
         this.phrase = phrase;
     }
 
+    // Setter method for setting the current letter value (unused)
     public void setCurrentLetterValue(int currentLetterValue) {
         this.currentLetterValue = currentLetterValue;
     }
 
+    // Method to set the current letter value randomly (unused)
     public void setLetterValue() {
         int randomInt = (int) ((Math.random() * 10) + 1) * 100;
         currentLetterValue = randomInt;
     }
 
+    // Method to check if a guess matches the original phrase
     public boolean isSolved(String guess) {
         return phrase.equals(guess);
     }
 
+    // Method to load a random phrase from a file
     private String loadPhrase() {
         String tempPhrase = "";
         int numOfLines = 0;
@@ -74,30 +84,34 @@ public class Board {
             System.out.println("Error reading or parsing phrases.txt");
         }
 
+        // Initialize solvedPhrase with underscores for each letter in the phrase
         for (int i = 0; i < tempPhrase.length(); i++) {
             if (tempPhrase.substring(i, i + 1).equals(" ")) {
-                solvedPhrase += "  ";
+                solvedPhrase += "  ";  // Preserve spaces in solvedPhrase
             } else {
-                solvedPhrase += "_ ";
+                solvedPhrase += "_ ";  // Replace letters with underscores
             }
         }
 
         return tempPhrase;
     }
 
+    // Method to guess a letter and update the solved phrase
     public boolean guessLetter(String guess) {
         boolean foundLetter = false;
         StringBuilder newSolvedPhrase = new StringBuilder();
 
+        // Iterate through the original phrase
         for (int i = 0; i < phrase.length(); i++) {
             if (phrase.substring(i, i + 1).equals(guess)) {
-                newSolvedPhrase.append(guess).append(" ");
+                newSolvedPhrase.append(guess).append(" ");  // Add the guessed letter
                 foundLetter = true;
             } else {
+                // Preserve existing solved letters or spaces
                 newSolvedPhrase.append(solvedPhrase.substring(i * 2, i * 2 + 1)).append(" ");
             }
         }
-        solvedPhrase = newSolvedPhrase.toString();
+        solvedPhrase = newSolvedPhrase.toString();  // Update the solved phrase
         return foundLetter;
     }
 }
--- PhraseSolver.java
+++ PhraseSolver.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 
+// Define the main class for the PhraseSolver game
 public class PhraseSolver {
-    private Player player1;
-    private Player player2;
-    private Board board;
-    private boolean gameSolved;
+    // Declare private class members
+    private Player player1;         // Player 1
+    private Player player2;         // Player 2
+    private Board board;            // Game board
+    private boolean gameSolved;    // Flag to check if the game is solved
 
+    // Constructor for initializing the game with player names
     public PhraseSolver(String playerName1, String playerName2) {
-        player1 = new Player(playerName1);
-        player2 = new Player(playerName2);
-        board = new Board();
-        gameSolved = false;
+        player1 = new Player(playerName1);   // Create Player 1 object
+        player2 = new Player(playerName2);   // Create Player 2 object
+        board = new Board();                // Create the game board
+        gameSolved = false;                 // Initialize gameSolved to false
     }
 
+    // Getter methods for accessing private members
     public Player getPlayer1() {
         return player1;
     }
@@ -29,6 +33,7 @@ public class PhraseSolver {
         return gameSolved;
     }
 
+    // Setter methods for modifying private members
     public void setPlayer1(Player player1) {
         this.player1 = player1;
     }
@@ -45,30 +50,38 @@ public class PhraseSolver {
         this.gameSolved = gameSolved;
     }
 
+    // Method to start and play the game
     public void play() {
-        boolean solved = false;
-        int currentPlayer = 1;
+        boolean solved = false;           // Flag to check if the game is solved
+        int currentPlayer = 1;           // Initialize the current player as Player 1
 
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);  // Create a Scanner object for user input
 
+        // Main game loop
         while (!solved) {
+            // Display the current state of the board
             System.out.println("Current Board: " + board.getSolvedPhrase());
 
+            // Determine which player's turn it is
             Player currentPlayerObj = (currentPlayer == 1) ? player1 : player2;
             Player otherPlayer = (currentPlayer == 1) ? player2 : player1;
 
+            // Prompt the current player for a letter guess
             System.out.print(currentPlayerObj.getName() + ", enter a letter guess: ");
             String guess = input.next();
 
+            // Check if the guess is correct and update the board
             boolean guessResult = board.guessLetter(guess);
 
             if (guessResult) {
                 System.out.println("Correct guess!");
             } else {
+                // Incorrect guess, switch to the other player's turn
                 System.out.println("Incorrect guess. " + otherPlayer.getName() + "'s turn.");
                 currentPlayer = (currentPlayer == 1) ? 2 : 1;
             }
 
+            // Check if the game has been solved
             if (board.getPhrase().equalsIgnoreCase(board.getSolvedPhrase().replaceAll(" ", ""))) {
                 solved = true;
                 System.out.println(currentPlayerObj.getName() + " wins!");
